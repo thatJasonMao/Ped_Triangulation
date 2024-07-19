ee1cbe5f191f9e354dc0bc2a36d13e4a309c8ba7
Lennon <maozixuan@yeah.net>
2024-07-19 10:39:17 +0800

    [Title  ] Add Config File And Script. [Project] Triangulation [Author ] Lennon

--- Assets/TriangulationUtils_NoHole/TestScene/NoHoleTestLogic.cs
+++ Assets/TriangulationUtils_NoHole/TestScene/NoHoleTestLogic.cs
@@ -1,18 +1,44 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class NoHoleTestLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Vector2> Points = new List<Vector2>();
+
+    private static string points_path = "Assets/StreamingAssets/BoundaryPoints.txt";
+
+    private StreamReader _reader = new StreamReader(points_path);
+
+    private void Awake()
     {
-        
+        LoadBoundaryPoints();
+        FreezeReader();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadBoundaryPoints()
     {
-        
+        int _counter = 0;
+        while (!_reader.EndOfStream)
+        {
+            string line = _reader.ReadLine();
+            _counter++;
+
+            var locations = line.Split(',');
+            string str_x = locations[0];
+            string str_y = locations[1];
+
+            Vector2 new_bd = new Vector2(float.Parse(str_x), float.Parse(str_y));
+            Debug.Log($"新增边界点 X: {new_bd.x} Y: {new_bd.y}");
+            Points.Add(new_bd);
+        }
+        Debug.Log($"读取完成 当前配置文件共读取 {_counter} 行");
+    }
+
+    private void FreezeReader()
+    {
+        _reader?.Close();
+        _reader = null;
     }
 }
